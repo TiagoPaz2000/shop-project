023ffc443461fc039b76ad8e75a82bda35cf72e9
tiago <tiagopaz.dev@gmail.com>
2022-04-06 18:36:43 -0300

    add: test if first name throw a error when received a number

--- src/app/domain/usecases/create-user.spec.ts
+++ src/app/domain/usecases/create-user.spec.ts
@@ -5,8 +5,11 @@ import User from '../entities/user';
 
 class CreateUser {
   create({ firstName, lastName, email, password }: User) {
+    if (typeof firstName !== 'string') return ({ error: '"firstName" must be a string' });
     if (firstName.length <= 3) return ({ error: '"firstName" need to have more than 3 length' });
+    return 'hello world';
   }
+
 }
 
 describe('Create User Test Suite', () => {
@@ -21,6 +24,19 @@ describe('Create User Test Suite', () => {
     const result = new CreateUser().create(sut);
 
     expect(result).to.be.deep.equal({ error: '"firstName" need to have more than 3 length' });
+  });
+
+  it('test if first name throw a error when received a number', () => {
+    const sut = {
+      firstName: 3,
+      lastName: 'validLastName',
+      email: 'valid@mail.com',
+      password: 'validPassword',
+    };
+
+    const result2 = new CreateUser().create(sut as any);
+
+    expect(result2).to.be.deep.equal({ error: '"firstName" must be a string' });
   });
 });
 
