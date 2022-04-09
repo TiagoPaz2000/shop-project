69c53d1b243508cfec2bf72a830a8c00345b78e3
tiago <tiagopaz.dev@gmail.com>
2022-04-09 15:20:16 -0300

    refactor: refactoring the create-user tests and module

--- src/app/domain/usecases/user/create-user.spec.ts
+++ src/app/domain/usecases/user/create-user.spec.ts
@@ -1,36 +1,45 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import User from '../../entities/user/user';
+import CreateUser from './create-user';
+import { IEmailValidator } from '../../validators/emailValidator';
 
-class CreateUser {
-  constructor(private validEmail: any) {
-    this.validEmail = validEmail;
+const makeSut = () => {
+  class EmailValidatorSpy implements IEmailValidator {
+    email: any;
+
+    valid(email: string) {
+      this.email = email;
+      if (email === 'invalidEmail') {
+        return false;
+      }
+
+      return true;
+    }
   }
 
-  create({ firstName, lastName, email, password }: User) {
-    if (typeof firstName !== 'string') return ({ error: '"firstName" must be a string' });
-    if (firstName.length <= 3) return ({ error: '"firstName" need to have more than 3 length' });
-    if (typeof lastName !== 'string') return ({ error: '"lastName" must be a string' });
-    if (lastName.length <= 3) return ({ error: '"lastName" need to have more than 3 length' });
-    if (!this.validEmail(email)) return ({ error: '"email" has a incorrect format' });
-    if (typeof password !== 'string') return ({ error: '"password" must be a string' });
-    if (password.length < 6) return ({ error: '"password" need to have more than 6 length' });
-
-    return new User({ firstName, lastName, email, password });
-  }
-}
+  return ({
+    EmailValidator: new EmailValidatorSpy(),
+  });
+};
 
 describe('Create User Test Suite', () => {
-  const validEmailStub = (email: string) => {
-    if (email === 'invalidEmail') {
-      return false;
-    }
-
-    return true;
-  };
-
   it('test if a new "User" is returned', () => {
+    const { EmailValidator } = makeSut();
+    const request = {
+      firstName: 'validFirstName',
+      lastName: 'validLastName',
+      email: 'valid@mail.com',
+      password: 'validPassword',
+    };
+
+    const result = new CreateUser(EmailValidator).create(request);
+
+    expect(result).to.be.deep.equal({ user: request, error: null });
+  });
+
+  it('test if email validator is called with correct values', () => {
+    const { EmailValidator } = makeSut();
     const sut = {
       firstName: 'validFirstName',
       lastName: 'validLastName',
@@ -38,12 +47,26 @@ describe('Create User Test Suite', () => {
       password: 'validPassword',
     };
 
-    const result = new CreateUser(validEmailStub).create(sut);
+    const result = new CreateUser(EmailValidator).create(sut);
+    expect(EmailValidator.email).to.be.equal('valid@mail.com');
+  });
 
-    expect(result).to.be.deep.equal(sut);
+  it('test if email throw a error when dont received a valid email', () => {
+    const { EmailValidator } = makeSut();
+    const request = {
+      firstName: 'validFirstName',
+      lastName: 'validLastName',
+      email: 'invalidEmail',
+      password: '12345',
+    };
+
+    const result = new CreateUser(EmailValidator).create(request);
+
+    expect(result).to.be.deep.equal({ error: '"email" has a incorrect format' });
   });
 
   it('test if first name is more than 3 length', () => {
+    const { EmailValidator } = makeSut();
     const sut = {
       firstName: '123',
       lastName: 'validLastName',
@@ -51,72 +74,77 @@ describe('Create User Test Suite', () => {
       password: 'validPassword',
     };
 
-    const result = new CreateUser(validEmailStub).create(sut);
+    const result = new CreateUser(EmailValidator).create(sut);
 
     expect(result).to.be.deep.equal({ error: '"firstName" need to have more than 3 length' });
   });
 
   it('test if first name throw a error when dont received a string', () => {
-    const sut = {
-      firstName: 3,
+    const { EmailValidator } = makeSut();
+    const request = {
+      firstName: 123,
       lastName: 'validLastName',
       email: 'valid@mail.com',
       password: 'validPassword',
     };
 
-    const result2 = new CreateUser(validEmailStub).create(sut as any);
+    const result2 = new CreateUser(EmailValidator).create(request as any);
 
     expect(result2).to.be.deep.equal({ error: '"firstName" must be a string' });
   });
 
-    it('test if last name is more than 3 length', () => {
-    const sut = {
+  it('test if last name is more than 3 length', () => {
+    const { EmailValidator } = makeSut();
+    const request = {
       firstName: 'validFirstName',
       lastName: '123',
       email: 'valid@mail.com',
       password: 'validPassword',
     };
 
-    const result = new CreateUser(validEmailStub).create(sut);
+    const result = new CreateUser(EmailValidator).create(request);
 
     expect(result).to.be.deep.equal({ error: '"lastName" need to have more than 3 length' });
   });
 
   it('test if last name throw a error when dont received a string', () => {
-    const sut = {
+    const { EmailValidator } = makeSut();
+    const request = {
       firstName: 'validFirstName',
-      lastName: 'validLastName',
-      email: 'invalidEmail',
+      lastName: 123,
+      email: 'valid@mail.com',
       password: 'validPassword',
     };
 
-    const result2 = new CreateUser(validEmailStub).create(sut as any);
+    const result2 = new CreateUser(EmailValidator).create(request as any);
 
-    expect(result2).to.be.deep.equal({ error: '"email" has a incorrect format' });
+    expect(result2).to.be.deep.equal({ error: '"lastName" must be a string' });
   });
 
   it('test if password is more than 6 length', () => {
-    const sut = {
+    const { EmailValidator } = makeSut();
+    const request = {
       firstName: 'validFirstName',
       lastName: 'validLastName',
       email: 'valid@mail.com',
       password: '12345',
     };
 
-    const result = new CreateUser(validEmailStub).create(sut);
+    const result = new CreateUser(EmailValidator).create(request);
 
     expect(result).to.be.deep.equal({ error: '"password" need to have more than 6 length' });
   });
 
   it('test if password throw a error when dont received a string', () => {
-    const sut = {
+    const { EmailValidator } = makeSut();
+    const request = {
       firstName: 'validFirstName',
       lastName: 'validLastName',
       email: 'valid@mail.com',
-      password: 0,
+      password: 12345,
     };
 
-    const result2 = new CreateUser(validEmailStub).create(sut as any);
+    const result2 = new CreateUser(EmailValidator).create(request as any);
 
     expect(result2).to.be.deep.equal({ error: '"password" must be a string' });
   });
