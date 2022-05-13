12efb1f5fb5d640ae2be5714a1690c8f38f388ee
Tiago Paz <paz@Tiagos-MacBook-Air.local>
2022-05-13 09:17:02 -0300

    test: should user validator is called with correct values

--- src/app/presentation/controllers/signup-controller.spec.ts
+++ src/app/presentation/controllers/signup-controller.spec.ts
@@ -1,20 +1,31 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { describe, it } from 'mocha';
 import { expect} from 'chai';
 import sinon from 'sinon';
 import Controller from '../protocols/controller';
-import { HttpRequest, HttpResponse } from '../protocols/http';
+import { HttpResponse } from '../protocols/http';
 import { IError, IUserValidator } from '../../domain/usecases/user/protocols';
 import User from '../../domain/entities/user/user';
 
+export namespace SignUpController {
+  export type Request = {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+  }
+}
+
+
 class SignUpController implements Controller {
-  constructor(private userValidator: any) {
+  constructor(private userValidator: IUserValidator) {
     this.userValidator = userValidator;
   }
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle(httpRequest: SignUpController.Request): Promise<HttpResponse> {
     const validUser = this.userValidator.valid(httpRequest);
     if (validUser.error) {
-      return ({ statusCode: validUser.status, body: {} });
+      return ({ statusCode: validUser.status || 500, body: {} });
     }
     return ({ statusCode: 200, body: {} });
   }
@@ -44,7 +55,7 @@ describe('SignUpController', () => {
       body: {},
     };
 
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest as any);
 
     expect(httpResponse.statusCode).to.equal(200);
   });
@@ -56,14 +67,34 @@ describe('SignUpController', () => {
 
     const httpRequest = {
       body: {
+        firstName: 'invalid_firstName',
         lastName: 'valid_lastName',
         email: 'valid_email',
         password: 'valid_password',
       },
     };
 
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest.body);
 
     expect(httpResponse.statusCode).to.equal(400);
+  });
+
+  it('Should valid user method is called with correct params', async () => {
+    const { sut, userValidator } = makeSut();
+
+    const userValidatorSpy = sinon.spy(userValidator, 'valid');
+
+    const httpRequest = {
+      body: {
+        firstName: 'valid_firstName',
+        lastName: 'valid_lastName',
+        email: 'valid_email',
+        password: 'valid_password',
+      },
+    };
+
+    await sut.handle(httpRequest.body);
+
+    expect(userValidatorSpy.calledWith(httpRequest.body)).to.be.true;
   });
 });
