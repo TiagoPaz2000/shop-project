e2dd79943edc5e889c0e0e7ca754e37c243fc65f
Tiago Paz <paz@Tiagos-MacBook-Air.local>
2022-05-23 14:50:03 -0300

    add: create INewAccount

--- src/app/domain/protocols/index.ts
+++ src/app/domain/protocols/index.ts
@@ -8,6 +8,10 @@ export interface IEmailExists {
   valid(email: User['email']): Promise<IError>
 }
 
+export interface INewAccount {
+  create(data: Omit<User, 'id'>): Promise<string>
+}
+
 export interface IError {
   error?: string
   status?: number
