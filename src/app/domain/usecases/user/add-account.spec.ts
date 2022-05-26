445bae5c682cac0e4ee157fb973a825d0aef7227
Tiago Paz <paz@Tiagos-MacBook-Air.local>
2022-05-25 23:55:39 -0300

    test: testing method create of user repository in add account

--- src/app/domain/usecases/user/add-account.spec.ts
+++ src/app/domain/usecases/user/add-account.spec.ts
@@ -1,29 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-namespace */
 import { describe, it } from 'mocha';
-import { expect} from 'chai';
+import { expect, use} from 'chai';
 import sinon from 'sinon';
 import User from '../../entities/user/user';
-import { INewAccount } from '../../protocols';
+import { INewAccount, IToken, ITokenGenerator, IUserRepository } from '../../protocols';
 import AddAccount from './add-account';
-
-// const makeAddAccount = (): INewAccount => {
-//   class AddAccountStub implements INewAccount {
-//     async create(data: Omit<User, 'id'>): Promise<string> {
-//       return new Promise(resolve => resolve('valid_token'));
-//     }
-//   }
-
-//   return new AddAccountStub();
-// };
-
-interface IToken {
-  token: string
-}
-
-interface ITokenGenerator {
-  create(userId: User['id']): IToken
-}
 
 const makeTokenGenerator = (): ITokenGenerator => {
   class TokenGeneratorStub implements ITokenGenerator {
@@ -34,10 +16,6 @@ const makeTokenGenerator = (): ITokenGenerator => {
 
   return new TokenGeneratorStub();
 };
-
-interface IUserRepository {
-  create(data: Omit<User, 'id'>): Promise<User>
-}
 
 const makeUserRepository = (): IUserRepository => {
   class UserRepositoryStub implements IUserRepository {
@@ -66,3 +44,39 @@ const makeSut = () => {
     userRepository,
   });
 };
+
+describe('SignUpController', () => {
+  it('Should method create of user repository called with correct data', async () => {
+    const { sut, userRepository } = makeSut();
+
+    const userRepositorySpy = sinon.spy(userRepository, 'create');
+
+    const fakeData: Omit<User, 'id'> = {
+      firstName: 'valid_firstName',
+      lastName: 'valid_lastName',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+
+    await sut.create(fakeData);
+
+    expect(userRepositorySpy.calledWith(fakeData)).to.be.true;
+  });
+
+    it('Should method create of user repository throw an error', async () => {
+    const { sut, userRepository } = makeSut();
+
+    sinon.stub(userRepository, 'create').throws();
+    const fakeData: Omit<User, 'id'> = {
+      firstName: 'valid_firstName',
+      lastName: 'valid_lastName',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    try {
+      await sut.create(fakeData);
+    } catch (error) {
+      expect(userRepository.create).to.throw();
+    }
+  });
+});
