dcc45d400e6b5790b7a0bb9b995fa7398ad18501
Tiago Paz <paz@Tiagos-MacBook-Air.local>
2022-05-25 23:54:57 -0300

    fix: change a types any to interfaces

--- src/app/domain/usecases/user/add-account.ts
+++ src/app/domain/usecases/user/add-account.ts
@@ -1,14 +1,14 @@
 import User from '../../entities/user/user';
-import { INewAccount } from '../../protocols';
+import { INewAccount, ITokenGenerator, IUserRepository } from '../../protocols';
 
 export default class AddAccount implements INewAccount {
-  constructor(private userRepository: any, private tokenGenerator: any) {
+  constructor(private userRepository: IUserRepository, private tokenGenerator: ITokenGenerator) {
     this.userRepository = userRepository;
     this.tokenGenerator = tokenGenerator;
   }
 
   async create(data: Omit<User, 'id'>): Promise<string> {
-    const newUser = await this.userRepository(data);
-    return this.tokenGenerator.create();
+    const newUser = await this.userRepository.create(data);
+    return this.tokenGenerator.create(newUser.id).token;
   }
 }
