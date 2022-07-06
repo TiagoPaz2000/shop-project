1011965a3130834e3520878dfd2b4dc249ee7f3a
Tiago Paz <tiagopaz.dev@gmail.com>
2022-07-06 12:56:05 -0300

    test: implements integration test - user signup

---
+++ docker-test/entrypoint.sh
@@ -0,0 +1,8 @@
+docker run -d \
+	--name porquinho-test \
+  -p 5432:5432 \
+  -e POSTGRES_USER=root \
+	-e POSTGRES_PASSWORD=docker \
+  -e POSTGRES_DB=porquinho_test \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	postgres
