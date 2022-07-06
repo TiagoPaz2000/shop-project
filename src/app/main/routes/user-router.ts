d46e4f99dc60bb6839e3ad7d4c68fbe7ede415bb
Tiago Paz <tiagopaz.dev@gmail.com>
2022-07-06 12:55:31 -0300

    fix: use statusCode in user router

--- src/app/main/routes/user-router.ts
+++ src/app/main/routes/user-router.ts
@@ -5,9 +5,9 @@ const userRouter = Router();
 
 userRouter.post('/register', async (req: Request, res: Response) => {
   const { firstName, lastName, email, password } = req.body;
-  const { body } = await signUpFactory().handle({ firstName, lastName, email, password });
+  const { body, statusCode } = await signUpFactory().handle({ firstName, lastName, email, password });
 
-  res.status(201).json({ body: { user: body } });
+  res.status(statusCode).json({ response: body });
 });
 
 export default userRouter;
